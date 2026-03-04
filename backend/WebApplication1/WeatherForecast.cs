a142658127d24f03563ef1958590cdabca87d79e
Tapan <tapan007sathwara@gmail.com>
2026-03-04 18:36:56 +0530

    chnages from dev2

--- backend/WebApplication1/WeatherForecast.cs
+++ backend/WebApplication1/WeatherForecast.cs
@@ -6,6 +6,7 @@ namespace WebApplication1
 
         public int TemperatureC { get; set; }
 
+        //chnages in The dev2 now take pull    from the dev1 and then merge to the main branch
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
